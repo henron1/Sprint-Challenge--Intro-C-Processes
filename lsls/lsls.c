268e58d2c334977c8909fa51f5887662efe6ea08
Henry Neal <henry.neal0@gmail.com>
2019-06-07 12:25:56 -0400

    parse cl and close dir

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <string.h>
 #include <sys/stat.h>
+
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
-  printf("There are %d arguments that exist:\n", argc);
-  // Open directory
   DIR *dir;
-  // Repeatly read and print entries
+  struct dirent *d;
 
+  // Parse command line
+  int i;
+
+    printf("There are %d command line argument(s):\n", argc);
+
+    for (i = 0; i < argc; i++) {
+        printf("   %s\n", argv[i]);
+    }
+
+    return 0;
+  // Open directory
+  dir = opendir();
+  // Repeatly read and print entries
   // Close directory
-  closedir(dir);
+  closedir();
   return 0;
 }
